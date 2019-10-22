d299db76643714466d88e52e8bce0bf5668dd355
Prithiviraj.R <itsprithiv@gmail.com>
2019-10-22 19:12:07 +0530

    to create project

---
+++ src/main/java/com/diyakart/App.java
@@ -0,0 +1,13 @@
+package com.diyakart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This site is under maintenance" );
+    }
+}
